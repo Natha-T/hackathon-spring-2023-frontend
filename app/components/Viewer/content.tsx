b88eb95b345ceb18972abae19441bd1c0bcf7037
guitarkeegan <keegananglim@gmail.com>
2023-06-10 06:11:22 -0700

    change font color on viewer content page to white

--- app/components/Viewer/content.tsx
+++ app/components/Viewer/content.tsx
@@ -5,7 +5,7 @@ import PubSecond from "./pubsecond";
 
 export default function Content() {
   return (
-    <div className=" bg-gray-800 flex gap-6 p-7">
+    <div className=" bg-gray-800 flex gap-6 p-7 text-white">
       <div className="grid space-y-4 w-3/4">
         <StreamerLink />
         <PoolDetail />
